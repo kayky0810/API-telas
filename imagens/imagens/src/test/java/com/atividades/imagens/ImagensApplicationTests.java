e3e8252a2e4e2bb046eb0ccb229a8c4abe1ad85c
kayky0810 <kaykyvitor.reissouza2007@gmail.com>
2025-06-05 21:16:21 -0300

    Add files via upload

---
+++ imagens/imagens/src/test/java/com/atividades/imagens/ImagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividades.imagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
